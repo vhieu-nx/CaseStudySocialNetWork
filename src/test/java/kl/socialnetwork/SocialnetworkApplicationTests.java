5f1b2dfa737d9a1e8573e2202df0db647cc2e7d4
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-06 16:23:02 +0700

    frist commit case

---
+++ src/test/java/kl/socialnetwork/SocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package kl.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
