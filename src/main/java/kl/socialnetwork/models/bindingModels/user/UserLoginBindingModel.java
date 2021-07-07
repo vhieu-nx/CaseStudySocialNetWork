26cec83d7f1754f7b06f5725ebde41430a62c37e
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-07 14:05:42 +0700

    userbindingmodel add contructor

---
+++ src/main/java/kl/socialnetwork/models/bindingModels/user/UserLoginBindingModel.java
@@ -0,0 +1,10 @@
+package kl.socialnetwork.models.bindingModels.user;
+
+public class UserLoginBindingModel {
+    private String username;
+    private String password;
+
+    public UserLoginBindingModel() {
+    }
+
+}
