e36ca3d91e17c94fe55c451460d08510e2c4e258
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-07 15:06:09 +0700

    build annotation passwordMatching, UinqueUserName and create class PasswordMatchingValidator and UniqueUsernameValidator

--- src/main/java/kl/socialnetwork/utils/constants/ValidationMessageConstants.java
+++ src/main/java/kl/socialnetwork/utils/constants/ValidationMessageConstants.java
@@ -3,4 +3,8 @@ package kl.socialnetwork.utils.constants;
 public class ValidationMessageConstants {
     private ValidationMessageConstants(){}
     public static final String INVALID_CREDENTIALS_MESSAGE = "Incorrect credentials.";
+    public static final String USER_INVALID_EMAIL_MESSAGE = "Invalid e-mail address.";
+    public static final String USER_INVALID_USERNAME_MESSAGE = "Username should be at least 4 and maximum 16 characters long.";
+    public static final String USER_INVALID_PASSWORD_MESSAGE = "Invalid Password format.";
+
 }
