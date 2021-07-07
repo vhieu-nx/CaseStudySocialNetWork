4605d3acc06c5af2839ae7ae9baf4497599bec03
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-07 14:19:18 +0700

    add package untils.constraints and add class ValidationMessageConstants

---
+++ src/main/java/kl/socialnetwork/utils/constants/ValidationMessageConstants.java
@@ -0,0 +1,6 @@
+package kl.socialnetwork.utils.constants;
+
+public class ValidationMessageConstants {
+    private ValidationMessageConstants(){}
+    public static final String INVALID_CREDENTIALS_MESSAGE = "Incorrect credentials.";
+}
