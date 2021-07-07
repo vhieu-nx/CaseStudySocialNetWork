67fddc7b127f504b46b2a07d343a73d55bddd17c
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-07 08:52:42 +0700

    delete all

--- src/main/java/kl/socialnetwork/SocialnetworkApplication.java
+++ src/main/java/kl/socialnetwork/SocialnetworkApplication.java
@@ -2,8 +2,12 @@ package kl.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class SocialnetworkApplication {
 
 	public static void main(String[] args) {
