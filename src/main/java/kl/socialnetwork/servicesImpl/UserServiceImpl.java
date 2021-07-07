d50ec18076f902d6219af90db50bc30c60815b59
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-07 14:51:08 +0700

    add repository RoleRepository

---
+++ src/main/java/kl/socialnetwork/servicesImpl/UserServiceImpl.java
@@ -0,0 +1,85 @@
+package kl.socialnetwork.servicesImpl;
+
+import kl.socialnetwork.domain.entities.User;
+import kl.socialnetwork.domain.models.serviceModels.UserServiceModel;
+import kl.socialnetwork.domain.models.viewModels.user.UserCreateViewModel;
+import kl.socialnetwork.domain.models.viewModels.user.UserDetailsViewModel;
+import kl.socialnetwork.domain.models.viewModels.user.UserEditViewModel;
+import kl.socialnetwork.repositories.UserRepository;
+import kl.socialnetwork.services.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserCreateViewModel createUser(UserServiceModel userRegisterBindingModel) {
+        return null;
+    }
+
+    @Override
+    public boolean updateUser(UserServiceModel userUpdateBindingModel, String loggedInUserId) throws Exception {
+        return false;
+    }
+
+    @Override
+    public UserServiceModel updateUserOnlineStatus(String userName, boolean changeToOnline) throws Exception {
+        return null;
+    }
+
+    @Override
+    public UserDetailsViewModel getById(String id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public UserEditViewModel editById(String id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public User getByEmailValidation(String email) {
+        return null;
+    }
+
+    @Override
+    public User getByUsernameValidation(String username) {
+        return null;
+    }
+
+    @Override
+    public List<UserServiceModel> getAllUsers(String userId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean promoteUser(String id) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean demoteUser(String id) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean deleteUserById(String id) throws Exception {
+        return false;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
