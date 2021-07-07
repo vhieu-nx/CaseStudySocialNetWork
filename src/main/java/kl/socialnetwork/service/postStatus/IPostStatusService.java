4fd967b851c903da1cd4c38d4728db650137007f
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-07 11:35:58 +0700

    service post status

---
+++ src/main/java/kl/socialnetwork/service/postStatus/IPostStatusService.java
@@ -0,0 +1,7 @@
+package kl.socialnetwork.service.postStatus;
+
+import kl.socialnetwork.model.PostStatus;
+import kl.socialnetwork.service.IService;
+
+public interface IPostStatusService extends IService<PostStatus> {
+}
