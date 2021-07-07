ca5e93831dbbb809b060a29cefb021f8b7419e12
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-07 10:52:24 +0700

    add funciton get get-all-post-by-user-id/{id}

--- src/main/java/kl/socialnetwork/controller/PostController.java
+++ src/main/java/kl/socialnetwork/controller/PostController.java
@@ -1,5 +1,6 @@
 package kl.socialnetwork.controller;
 
+import kl.socialnetwork.model.AppUser;
 import kl.socialnetwork.model.Post;
 import kl.socialnetwork.service.post.IPostService;
 import kl.socialnetwork.service.user.IUserService;
@@ -8,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 public class PostController {
     @Autowired
@@ -20,6 +23,12 @@ public class PostController {
     public ResponseEntity<Post> getPostById(@PathVariable Long id) {
         Post post = postService.findById(id);
         return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+    @GetMapping("/get-all-post-by-user-id/{id}")
+    public ResponseEntity<List<Post>> getAllPostByUserId(@PathVariable Long id) {
+        AppUser guestUser = userService.findById(id);
+
+        return new ResponseEntity<>(postService.findAllByAppUser(guestUser), HttpStatus.OK);
     }
 
 }
