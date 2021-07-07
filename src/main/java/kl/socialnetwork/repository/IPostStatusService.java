d471c74c7b9858e7a81620fd3638301f90936056
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-07 11:29:13 +0700

    add function IPostStatusService

---
+++ src/main/java/kl/socialnetwork/repository/IPostStatusService.java
@@ -0,0 +1,7 @@
+package kl.socialnetwork.repository;
+
+import kl.socialnetwork.model.PostStatus;
+import kl.socialnetwork.service.IService;
+
+public interface IPostStatusService extends IService<PostStatus> {
+}
