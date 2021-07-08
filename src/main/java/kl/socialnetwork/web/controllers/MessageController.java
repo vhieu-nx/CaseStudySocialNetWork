d41c42703694af3550c082d64ed80e3b2bb3694a
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-08 23:22:18 +0700

    message api getallbyid

--- src/main/java/kl/socialnetwork/web/controllers/MessageController.java
+++ src/main/java/kl/socialnetwork/web/controllers/MessageController.java
@@ -1,12 +1,26 @@
 package kl.socialnetwork.web.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kl.socialnetwork.domain.models.bindingModels.message.MessageCreateBindingModel;
+import kl.socialnetwork.domain.models.serviceModels.MessageServiceModel;
+import kl.socialnetwork.domain.models.viewModels.message.MessageAllViewModel;
+import kl.socialnetwork.domain.models.viewModels.message.MessageFriendsViewModel;
 import kl.socialnetwork.services.MessageService;
+import kl.socialnetwork.utils.responseHandler.exceptions.CustomException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static kl.socialnetwork.utils.constants.ResponseMessageConstants.*;
 
 @RestController()
 @RequestMapping(value = "/message")
@@ -16,6 +30,7 @@ public class MessageController {
     private final MessageService messageService;
     private final ModelMapper modelMapper;
     private final ObjectMapper objectMapper;
+
     @Autowired
     public MessageController(SimpMessagingTemplate template, MessageService messageService, ModelMapper modelMapper, ObjectMapper objectMapper) {
         this.template = template;
@@ -23,4 +38,19 @@ public class MessageController {
         this.modelMapper = modelMapper;
         this.objectMapper = objectMapper;
     }
+
+    @GetMapping(value = "/all/{id}")
+    public List<MessageAllViewModel> getAllMessages(@PathVariable(value = "id") String chatUserId, Authentication principal) {
+        String loggedInUsername = principal.getName();
+
+        List<MessageServiceModel> messageServiceModels = this.messageService.getAllMessages(loggedInUsername, chatUserId);
+
+        return messageServiceModels.stream()
+                .map(messageServiceModel -> modelMapper.map(messageServiceModel, MessageAllViewModel.class))
+                .collect(Collectors.toList());
+    }
+
+
+
+
 }
