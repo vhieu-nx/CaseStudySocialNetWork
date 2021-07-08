25de9f3e513fab6d9ef6dee0cb16628b416436ac
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-08 22:59:28 +0700

    websocket config error no bean and done autowired SimpMessagingTemplate

--- src/main/java/kl/socialnetwork/config/ApplicationSocketSecurityConfiguration.java
+++ src/main/java/kl/socialnetwork/config/ApplicationSocketSecurityConfiguration.java
@@ -1,10 +1,10 @@
 package kl.socialnetwork.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
-@Configurable
+@Configuration
 public class ApplicationSocketSecurityConfiguration extends AbstractSecurityWebSocketMessageBrokerConfigurer {
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
---
+++ src/main/java/kl/socialnetwork/web/websocket/WebSocketEventName.java
@@ -0,0 +1,17 @@
+package kl.socialnetwork.web.websocket;
+
+public enum WebSocketEventName {
+    SUBSCRIBE(""),
+    CONNECT("/chat/login"),
+    DISCONNECT("/chat/logout"),
+    MESSAGE("");
+    private String destination;
+
+    WebSocketEventName(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDestination() {
+        return this.destination;
+    }
+}
