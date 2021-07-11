8a9eb33fa3837ed2693f86d5193654c80d78a98d
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-11 22:05:52 +0700

    createComment_whenUsersAndPostAndCommentCreateBindingModelAreValid_createComment

---
+++ src/test/java/kl/socialnetwork/servicesImpl/CommentServiceTests.java
@@ -0,0 +1,95 @@
+package kl.socialnetwork.servicesImpl;
+
+import kl.socialnetwork.domain.entities.Comment;
+import kl.socialnetwork.domain.entities.Post;
+import kl.socialnetwork.domain.entities.User;
+import kl.socialnetwork.domain.models.bindingModels.comment.CommentCreateBindingModel;
+import kl.socialnetwork.repositories.CommentRepository;
+import kl.socialnetwork.repositories.PostRepository;
+import kl.socialnetwork.repositories.UserRepository;
+import kl.socialnetwork.services.CommentService;
+import kl.socialnetwork.testUtils.CommentsUtils;
+
+import kl.socialnetwork.validations.serviceValidation.services.CommentValidationService;
+import kl.socialnetwork.validations.serviceValidation.services.PostValidationService;
+import kl.socialnetwork.validations.serviceValidation.services.UserValidationService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static kl.socialnetwork.utils.constants.ResponseMessageConstants.SERVER_ERROR_MESSAGE;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentServiceTests {
+
+    @Autowired
+    private CommentService commentService;
+
+    @MockBean
+    private PostRepository mockPostRepository;
+
+    @MockBean
+    private UserRepository mockUserRepository;
+
+    @MockBean
+    private CommentRepository mockCommentRepository;
+
+    @MockBean
+    private PostValidationService mockPostValidationService;
+
+    @MockBean
+    private UserValidationService mockUserValidationService;
+
+    @MockBean
+    private CommentValidationService mockCommentValidationService;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUpTest() {
+
+    }
+
+    @Test
+    public void createComment_whenUsersAndPostAndCommentCreateBindingModelAreValid_createComment() throws Exception {
+        // Arrange
+        CommentCreateBindingModel commentCreateBindingModel = CommentsUtils.getCommentCreateBindingModel(1).get(0);
+
+        when(mockCommentValidationService.isValid(any(CommentCreateBindingModel.class)))
+                .thenReturn(true);
+
+        when(mockPostValidationService.isValid(any(Post.class)))
+                .thenReturn(true);
+
+        when(mockUserValidationService.isValid(any(User.class))).thenReturn(true);
+
+        when(mockUserRepository.findById(any()))
+                .thenReturn(java.util.Optional.of(new User()));
+
+        when(mockPostRepository.findById(any()))
+                .thenReturn(java.util.Optional.of(new Post()));
+
+        // Act
+        commentService.createComment(commentCreateBindingModel);
+
+        // Assert
+        verify(mockCommentRepository).save(any());
+        verifyNoMoreInteractions(mockCommentRepository);
+    }
+
+
+}
